d83b132e3c5016381ef05f1afdaec3879b64652a
Fabrizio Gutiérrez <gutierrezfabrizio03@gmail.com>
2023-07-10 14:57:09 -0430

    Probando cambios

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import employeesRouter from './routes/employees.routes.js';
 const app = express();
 
 app.use(express.json());
+
 app.use('/api', employeesRouter);
 app.use((req, res, next) => {
    return res.status(404).json({
